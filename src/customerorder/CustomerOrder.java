9c7e6a614c60a6d4f72dd3451030162d8e41be31
1895250 <1895250@E21025.CEGEPGIM.LCL>
2019-02-09 10:57:00 -0500

    All  class acessed in main

--- src/customerorder/CustomerOrder.java
+++ src/customerorder/CustomerOrder.java
@@ -23,8 +23,39 @@ public class CustomerOrder {
         o.setOrderDate("2019-2-9");
         o.setOrderNumber("a12bz");
         
-        System.out.println("Order Date"+ o.getOrderDate());
-        System.out.println("Order Number"+ o.getOrderNumber());
+        System.out.println("Order Date : "+ o.getOrderDate());
+        System.out.println("Order Number : "+ o.getOrderNumber());
+        
+        
+        
+        
+        
+        
+        Custmore c = new Custmore("Kamaldeep", "Lassal");
+        
+        System.out.println("Customer Name : "+ c.getcnaame());
+        System.out.println("Customer Locationr : "+ c.getclocation());
+        
+       
+        
+        
+        
+        
+        
+        SpecialOrder so = new SpecialOrder("2019-2-5","189","2019-2-9","1895a");
+
+        System.out.println("Special Order Date : "+ so.GetofDate());
+        System.out.println("Special Order Number : "+ so.getofnumber());
+        
+        
+        
+        
+        
+        
+        NormalOrder no = new NormalOrder("2019-2-7","1895250","2019-2-9","1895a");
+        
+          System.out.println("Normal Order Date : "+ no.getODate());
+        System.out.println("Normal Order number : "+ no.getOnumber());
         
         
         
